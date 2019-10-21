1998856456cb5c78485b9ff16d697eba3c1532ce
DevUrielMC <urimarcervantes.95@gmail.com>
2019-10-21 15:42:00 -0500

    first commit

---
+++ src/com/exercise17exeptionhandling/app/MyExeptions.java
@@ -0,0 +1,24 @@
+package com.exercise17exeptionhandling.app;
+
+public class MyExeptions {
+
+	public static void main(String[] args) {
+		int[]numbers = {1,2,3,4,5,6,7,8,9,10};
+		
+		System.out.println("Element 1 is: "+numbers[0]);
+		System.out.println("Element 2 is: "+numbers[1]);
+		System.out.println("Element 3 is: "+numbers[2]);
+		System.out.println("Element 4 is: "+numbers[3]);
+		System.out.println("Element 5 is: "+numbers[4]);
+		System.out.println("Element 6 is: "+numbers[5]);
+		System.out.println("Element 7 is: "+numbers[6]);
+		System.out.println("Element 8 is: "+numbers[7]);
+		System.out.println("Element 9 is: "+numbers[8]);
+		System.out.println("Element 10 is: "+numbers[9]);
+		System.out.println("Element 11 is: "+numbers[10]);// EXEPTION
+		System.out.println("Element 1 is: "+numbers[0]);
+		System.out.println("Element 2 is: "+numbers[1]);
+		System.out.println("Element 3 is: "+numbers[2]);
+	}
+
+}
